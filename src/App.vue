346b671974fa6f59c8690599b53f328fa53e160a
Lucas Berthet <lu.berthet@gmail.com>
2023-11-29 12:06:30 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import FirstComponent from './components/FirstComponent.vue'
 import MagicButton from './components/MagicButton.vue'
 import Footer from './components/Footer.vue';
+import Header from './components/Header.vue'; 
 
 </script>
 
 <template>
+    <Header />
   <div class="flex flex-col justify-center items-center">
   <first-component title="My first Vue App" />
   <magic-button text="Dare click on me 😉" />

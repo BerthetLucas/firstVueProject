0829d1c817c16d549bb54b3ff8d57f10f57ced83
Lucas Berthet <lu.berthet@gmail.com>
2023-11-29 10:16:27 +0100

    trying to add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <script setup>
-import FirstComponent from './components/FirstComponent.vue';
-import MagicButton from './components/MagicButton.vue';
-
+import FirstComponent from './components/FirstComponent.vue'
+import MagicButton from './components/MagicButton.vue'
+import Footer from './components/Footer.vue';
 
 </script>
 
-
-
 <template>
-
-<first-component title="My first Vue App"/>
-<magic-button text="Dare click on me 😉"/>
-
-
+  <first-component title="My first Vue App" />
+  <magic-button text="Dare click on me 😉" />
+  <Footer />
 </template>
 
 <style scoped>
-
-
+@import './assets/base.css';
+@import './assets/main.css';
 </style>
